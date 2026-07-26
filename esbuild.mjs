d20f8d8f1d87d7d10b4120c0d8bc87bf12f68a68
GusDev <gukavo2011@gmail.com>
2026-07-25 23:05:28 -0300

    Adds support for the monaco-editor

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,6 +1,5 @@
 import { build } from "esbuild";
 import { cpSync } from "fs";
-import { cp } from "fs/promises";
 
 build({
     entryPoints: [
@@ -25,8 +24,24 @@ build({
     bundle: true,
     platform: "browser",
     external: ["electron", "path", "child_process", "fs", "fs/promises"],
+    loader: {".ttf": "file"},
+});
+
+build({
+    entryPoints: {
+        "editor.worker": "node_modules/monaco-editor/esm/vs/editor/editor.worker.js",
+        "json.worker": "node_modules/monaco-editor/esm/vs/language/json/json.worker.js",
+        "css.worker": "node_modules/monaco-editor/esm/vs/language/css/css.worker.js",
+        "html.worker": "node_modules/monaco-editor/esm/vs/language/html/html.worker.js",
+        "ts.worker": "node_modules/monaco-editor/esm/vs/language/typescript/ts.worker.js",
+    },
+    bundle: true,
+    format: "iife",           // <- script clássico, sem "type: module"
+    platform: "browser",
+    outdir: "dist/monaco-workers",
 });
 
 cpSync("src/main/index.html", "dist/main/index.html", {recursive: true});
 cpSync("src/core/ui/ask/", "dist/core/ui/ask", {recursive: true});
 cpSync("src/core/ui/warn/", "dist/core/ui/warn", { recursive: true});
+cpSync("node_modules/monaco-editor/min/vs", "dist/vs", {recursive: true});
